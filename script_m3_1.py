a40c8f0b8c9cdd5f4d7cf60194cad903be7a297f
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-10-30 23:19:18 +0900

    l,23:19

--- script_m3_1.py
+++ script_m3_1.py
@@ -3,6 +3,7 @@ NEAREST = Image.Resampling.NEAREST
 import numpy as np
 from pathlib import Path
 from tqdm import tqdm
+from functools import lru_cache
 
 rgbs = list({tuple(rgb) for rgb in np.random.RandomState(0).randint(0, 255, (10**7,3))})
 invs = {rgb:i for i, rgb in enumerate(rgbs)}
@@ -39,9 +40,9 @@ fin = Image.fromarray(fin_arr)
 fin
 
 me_arr = np.array(Image.open(r"/Users/qzrp0/gdrive/alumni/cigarette/me0.png").convert("RGB"))
-
 goods = list(Path("outputs/good").glob("*machine.png"))
-def c(i, j):
+
+def c(i, j, thr):
     if i in range(5):
         z = 0
         for d in ["human", "good"]:
@@ -52,25 +53,34 @@ def c(i, j):
     if i in range(8, 55):
         bij = b(i, j)
         m = me_arr[np.where((fin_arr == rgbs[bij]).all(axis=2))].mean()
-        if m < 254:
+#        if m < 254:
+        if m < 128:
             return Image.open(np.random.RandomState(bij).choice(np.array(brightness[:100]).transpose()[1]))
-        return Image.open(np.random.RandomState(bij).choice(np.array(brightness[-100:]).transpose()[1]))
+#        return Image.open(np.random.RandomState(bij).choice(np.array(brightness[-100:]).transpose()[1]))
     return Image.open(fnames[b(i,j)%len(fnames)])
 
 
-# 完成品
-out2 = list()
-for i in tqdm(range(30)):
+def d(i, pbar, thr=1000):
     tmp2 = list()
     for j in range(a(i)):
-        tmp2.append(c(i, j))
+        tmp2.append(c(i, j, thr))
+        pbar.update(n=1)
+        pbar.set_description(f"{i} {j}")
     img = Image.fromarray(np.hstack(tmp2))
     img2 = img.resize((ssize[0], round(ssize[0]*img.size[1]/img.size[0])), resample=NEAREST)
-#    len({tuple(rgb) for rgb in np.array(img2).reshape(-1, 3)})
-    out2.append(np.array(img2))
-fin2_arr = np.vstack(out2)
-fin2 = Image.fromarray(fin2_arr)
-fin2
+    return img2
+
+
+# 完成品
+N = 55
+with tqdm(total=b(N, 0)) as pbar:
+    out2 = list()
+    for i in range(N):
+        img2 = d(i, pbar)
+        out2.append(img2)
+    fin2_arr = np.vstack(out2)
+    fin2 = Image.fromarray(fin2_arr)
+    fin2
 
 
 def search():
