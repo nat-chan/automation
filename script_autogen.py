9bc598c4a204b4325dc8bedc77c86e209ea0429a
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-10-30 14:01:28 +0900

    pbarでbatchsize指定してit/sを表示

--- script_autogen.py
+++ script_autogen.py
@@ -1,15 +1,14 @@
 import win32gui
 import pyautogui
 import numpy as np
+from tqdm import tqdm
+from itertools import count
 import time
 
 col = [
     np.array([[[209,  99,  24]]], dtype=np.uint8),
     np.array([[[179, 192, 203]]], dtype=np.uint8),
 ]
-
-pos = pyautogui.position()
-print(f"{pos=}")
 
 def f1(pos):
     _pos = pyautogui.position()
@@ -28,13 +27,24 @@ def f2():
     pyautogui.press("enter")
     win32gui.SetForegroundWindow(_hwnd)
 
-while True:
+pos = pyautogui.position()
+print(f"{pos=}")
+
+batchsize = 8
+
+pbar = tqdm()
+for c in count():
     img = pyautogui.screenshot(region=(pos[0],pos[1], 1, 1))
     arr = np.array(img)
     if ((col[0]-arr)**2).mean()**.5 < 1:
         f2()
+        pbar.update(n=batchsize)
+        pbar.set_description(f"ready {c} {pbar.n}")
+
     elif ((col[1]-arr)**2).mean()**.5 < 1:
-        time.sleep(1)
+        pbar.update(n=0)
+        pbar.set_description(f"wait {c} {pbar.n}")
     else:
-        print("\x1b[31mSomething Wring\x1b[m")
+        print("\x1b[31mSomething went wrong\x1b[m")
         break
+    time.sleep(1)
